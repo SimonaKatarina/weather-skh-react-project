7fb7b5a98d829b45f73d3dad341e08f17e6fab92
Simona Katarina Hlinka <simonakatarinah@gmail.com>
2024-09-21 01:39:41 +0200

    Bootstrap added into the project
    Using `npm i bootstrap` I added Bootstrap into the project.
    
    In App.js added my content into a class="container".

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div class="container">
       Weather App
-      <br />
+      
       <footer>
         This project was coded by <a href='https://simona-katarina-portfolio.netlify.app/' target='_blank' rel="noopener noreferrer">Simona Katarína Hlinka </a>and is 
       <a href='https://github.com/SimonaKatarina/weather-skh-react-project/blob/master/README.md' target="_blank" rel="noopener noreferrer"> open-sourced on GitHub</a>
-    </footer></div>
+    </footer></div></div>
   );
 }
 
