60f5dbae0389e24e134eace51ab4e6844ffcbc71
Simona Katarina Hlinka <simonakatarinah@gmail.com>
2024-09-21 01:21:57 +0200

    Adding a footer with the repository linked
    In the App.js edited the function Apps return where I added a footer
    in which I included two links
    to access information about me and this repository project.
    Link to my portfolio ("Simona K... H...") and GitHub documentation link ("open-sourced on GitHub").
    
    Using the App.js as the main component for this application.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
-function App() {
+
+
+export default function App() {
   return (
     <div className="App">
       Weather App
-    </div>
+      <br />
+      <footer>
+        This project was coded by <a href='https://simona-katarina-portfolio.netlify.app/' target='_blank' rel="noopener noreferrer">Simona Katarína Hlinka </a>and is 
+      <a href='https://github.com/SimonaKatarina/weather-skh-react-project/blob/master/README.md' target="_blank" rel="noopener noreferrer"> open-sourced on GitHub</a>
+    </footer></div>
   );
 }
 
-export default App;
+
