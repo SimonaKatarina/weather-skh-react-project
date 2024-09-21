e96888413e72e95c45a2bc31688ad34a2f51c0f2
Simona Katarina Hlinka <simonakatarinah@gmail.com>
2024-09-21 20:39:17 +0200

    Weather.js working search engine
    Using state to store city information and event listeners to change when typing in a new city.
    Form onSubmit to handleSubmit. -search
    Input search onChange to handleCityChange - update city
    
    API call into a search function easier to use call.

--- src/Weather.js
+++ src/Weather.js
@@ -5,7 +5,7 @@ import axios from "axios";
 
 export default function Weather(props){
     const[weatherData, setWeatherData]=useState({ready:false});
-
+    const[city, setCity]=useState(props.defaultCity);
 
 function handleResponse(response){
     console.log(response.data);
@@ -15,21 +15,38 @@ function handleResponse(response){
         humidity: response.data.main.humidity,
         date: new Date(response.data.dt * 1000),
         description: response.data.weather[0].description,
-        iconUrl:"https://s3.amazonaws.com/shecodesio-production/uploads/files/000/144/453/original/sunny.png?1726893583",
+        iconUrl:`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
         wind:response.data.wind.speed,
         city:response.data.name
     });
 }
 
+function search(){
+let apiKey="96771e971243152d6b8948878c26adde";
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(handleResponse);
+}
+   
 
+
+function handleSubmit(event){
+    event.preventDefault();
+    search();
+
+}
+
+function handleCityChange(event){
+setCity(event.target.value);
+}
 
 if (weatherData.ready){
     return (
     <div className="Weather">
-        <form>
+        <form onSubmit={handleSubmit}>
     <div className="row">
         <div className="col-9">
-            <input type="search" placeholder="Enter a city..." className="form-control"  autoFocus="on" /></div>
+            <input type="search" placeholder="Enter a city..." className="form-control"  autoFocus="on" 
+            onChange={handleCityChange}/></div>
         <div className="col-3">
                 <input type="submit" value="Search" className="btn btn-primary w-100"/>
        </div>
@@ -38,10 +55,7 @@ if (weatherData.ready){
       <WeatherInfo  data={weatherData}/>
     </div> );
 } else {
-let apiKey="96771e971243152d6b8948878c26adde";
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(handleResponse);}
-
-
-return (" Loading...");
+search();
+return `Loading...`;
+}
 }
