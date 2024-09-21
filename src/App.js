c64acf518a9194dbf05ab40218909f0d572c99b2
Simona Katarina Hlinka <simonakatarinah@gmail.com>
2024-09-21 08:15:46 +0200

    API and AXIOS added
    Imported Axios and created an API call.
    Using useState to create JS closure weatherData, setWeatherData in which all the data is stored from receiving the data.
    If and else statements to show content depending if its ready(true).
    Default City also created for the first loading of the page.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <div className="App">
       <div class="container">
-      <Weather />
+      <Weather defaultCity="Rotterdam"/>
       <footer>
         This project was coded by <a href='https://simona-katarina-portfolio.netlify.app/' target='_blank' rel="noopener noreferrer">Simona Katarína Hlinka </a>and is 
       <a href='https://github.com/SimonaKatarina/weather-skh-react-project/blob/master/README.md' target="_blank" rel="noopener noreferrer"> open-sourced on GitHub</a>
--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Weather.css";
+import axios from "axios";
 
-export default function Weather(){
-    return <div className="Weather">
+export default function Weather(props){
+    const[weatherData, setWeatherData]=useState({ready:false});
+
+
+function handleResponse(response){
+    console.log(response.data);
+    setWeatherData({
+        ready: true,
+        temperature:response.data.main.temp,
+        humidity: response.data.main.humidity,
+        date: "Friday 09:00",
+        description: response.data.weather[0].description,
+        iconUrl:"https://s3.amazonaws.com/shecodesio-production/uploads/files/000/144/453/original/sunny.png?1726893583",
+        wind:response.data.wind.speed,
+        city:response.data.name
+    });
+}
+
+
+
+if (weatherData.ready){
+    return (
+    <div className="Weather">
         <form>
     <div className="row">
         <div className="col-9">
@@ -16,15 +38,20 @@ export default function Weather(){
             <div className="col-8">
         <h1>New York</h1>
         <ul>
-            <li>Friday 09:00, Broken clouds</li>
-            <li>Humidity 80% | Wind 13 km/h</li>
+            <li className="text-capitalize">{weatherData.date}, {weatherData.description}</li>
+            <li>Humidity: {weatherData.humidity}% | Wind: {weatherData.wind} km/h</li>
         </ul></div>
         <div className="col-4">
-            <img className="forecast-icon" src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="Sunny icon"/>
-            <span className="temperature"> 25  <span className="unit">°C</span></span>
+            <img className="forecast-icon" src={weatherData.iconUrl} alt={weatherData.description}/>
+            <span className="temperature"> {Math.round(weatherData.temperature)}  <span className="unit">°C</span></span>
             </div>
-            
         </div>
-    </div>
-    
+    </div> );
+} else {
+let apiKey="96771e971243152d6b8948878c26adde";
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(handleResponse);}
+
+
+return (" Loading...");
 }
