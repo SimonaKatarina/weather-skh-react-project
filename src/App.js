91ac90ae88b572989159eb80e368e459cf47ecc6
Simona Katarina Hlinka <simonakatarinah@gmail.com>
2024-09-21 06:02:59 +0200

    CSS styling added
    Linked new font styles into the index.html.
    Imported a background image into the files.
     Added CSS styling in the App.css and Weather.css - font- family, font-size, color, background, width,
    positioning, images.....
    
    Styled the application to my liking.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Weather from "./Weather";
 import './App.css';
 
 
+
 export default function App() {
   return (
     <div className="App">
--- src/Weather.js
+++ src/Weather.js
@@ -1,28 +1,27 @@
 import React from "react";
 import "./Weather.css";
-import "./sunny.png";
 
 export default function Weather(){
     return <div className="Weather">
         <form>
     <div className="row">
         <div className="col-9">
-            <input type="search" placeholder="Enter a city..." className="form-control"/></div>
+            <input type="search" placeholder="Enter a city..." className="form-control"  autoFocus="on" /></div>
         <div className="col-3">
-                <input type="submit" value="Search" className="btn btn-primary"/>
+                <input type="submit" value="Search" className="btn btn-primary w-100"/>
        </div>
    </div>
       </form>
         <div className="row">
-            <div className="col-6">
+            <div className="col-8">
         <h1>New York</h1>
         <ul>
             <li>Friday 09:00, Broken clouds</li>
             <li>Humidity 80% | Wind 13 km/h</li>
         </ul></div>
-        <div className="col-6">
-            <img src="src/sunny.png" alt="Sunny icon"/>
-             25°C
+        <div className="col-4">
+            <img className="forecast-icon" src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="Sunny icon"/>
+            <span className="temperature"> 25  <span className="unit">°C</span></span>
             </div>
             
         </div>
